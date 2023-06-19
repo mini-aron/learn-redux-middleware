004c8cb23cecc99a89498691d91f44dca0272cf8
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-06-19 20:10:39 +0900

    redux-logger 사용

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,4 +1,4 @@
-import { createAction,handleActions } from "redux-actions";
+import { createAction } from "redux-actions";
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
