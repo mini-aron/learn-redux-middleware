cb79bed25180761c414a5bf8ccfa56b46ef0829b
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-07-11 14:37:52 +0900

    refactor: loading store 수정

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from "redux-actions";
 import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 //액션타입 선언
 //한 요청당 세 개를 만들어야함
@@ -15,41 +16,45 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 // thunk 함수 생성
 // thunk 함수 내부에서는 시작할 때 , 성공했을 때, 실패했을 때 다른 액션을 디스패치 한다.
 
-export const getPost = id => async dispatch => {
-    dispatch({ type: GET_POST }); //요청을 시작한걸 알림
-    try {
-        const response = await api.getPost(id);
-        dispatch({
-            type: GET_POST_SUCCESS,
-            payload: response.data
-        });
-    } catch (e) {
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true //에러발생
-        }); //나중에 컴포넌트 단에서 에러를 조회할 수 있게 해 줌
-        throw e;
-    }
-};
+// export const getPost = id => async dispatch => {
+//     dispatch({ type: GET_POST }); //요청을 시작한걸 알림
+//     try {
+//         const response = await api.getPost(id);
+//         dispatch({
+//             type: GET_POST_SUCCESS,
+//             payload: response.data
+//         });
+//     } catch (e) {
+//         dispatch({
+//             type: GET_POST_FAILURE,
+//             payload: e,
+//             error: true //에러발생
+//         }); //나중에 컴포넌트 단에서 에러를 조회할 수 있게 해 줌
+//         throw e;
+//     }
+// };
 
-export const  getUsers = () => async dispatch => {
-    dispatch({ type: GET_USERS }); //요청을 시작한걸 알림
-    try {
-        const response = await api.getUsers();
-        dispatch({
-            type: GET_USERS_SUCCESS,
-            payload: response.data
-        }); //요청성공
-    } catch (e) {
-        dispatch({
-            type: GET_USERS_FAILURE,
-            payload: e,
-            error: true
-        }); //에러발생
-        throw e; //나중에 컴포넌트 단에서 에러를 조회할 수 있게 해 줌
-    }
-}
+// export const  getUsers = () => async dispatch => {
+//     dispatch({ type: GET_USERS }); //요청을 시작한걸 알림
+//     try {
+//         const response = await api.getUsers();
+//         dispatch({
+//             type: GET_USERS_SUCCESS,
+//             payload: response.data
+//         }); //요청성공
+//     } catch (e) {
+//         dispatch({
+//             type: GET_USERS_FAILURE,
+//             payload: e,
+//             error: true
+//         }); //에러발생
+//         throw e; //나중에 컴포넌트 단에서 에러를 조회할 수 있게 해 줌
+//     }
+// }
+
+export const getPost = createRequestThunk(GET_POST,api.getPost);  
+export const getUsers = createRequestThunk(GET_USERS,api.getUsers);   //완전 줄어듬!!!!!!
+
 
 //초기상태를 선언 
 //요청의 로딩 중 상태는 loading이라는 객체에서 관리한다.
